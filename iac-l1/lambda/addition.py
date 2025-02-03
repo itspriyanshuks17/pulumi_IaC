379cfd5346bce4a8b497106ef837c06f42378a56
PriyanshuKSharma <priyanshu17ks@gmail.com>
2025-02-03 18:37:42 +0530

    Updated addition file

--- iac-l1/lambda/addition.py
+++ iac-l1/lambda/addition.py
@@ -1,7 +1,10 @@
 import json
 
 def lambda_handler(event, context):
+    a = 10
+    b = 20
+    c = a + b
     return {
         "statusCode": 200,
-        "body": json.dumps({"message": "Lambda executed successfully!"})
+        "body": json.dumps({"result": c})
     }
