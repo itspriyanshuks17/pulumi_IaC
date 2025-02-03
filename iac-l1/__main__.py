db5291d1af19f3f25c818c0e3b82434297f177f8
PriyanshuKSharma <priyanshu17ks@gmail.com>
2025-02-03 15:30:02 +0530

    Role added for giving complete access of IAM

--- iac-l1/__main__.py
+++ iac-l1/__main__.py
@@ -1,10 +1,38 @@
-"""An AWS Python Pulumi program"""
-
 import pulumi
-from pulumi_aws import s3
+import pulumi_aws as aws
+import json
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.BucketV2('my-bucket')
+# IAM Role for Lambda
+lambda_role = aws.iam.Role("lambdaRole",
+    assume_role_policy=json.dumps({
+        "Version": "2012-10-17",
+        "Statement": [{
+            "Action": "sts:AssumeRole",
+            "Effect": "Allow",
+            "Principal": {"Service": "lambda.amazonaws.com"}
+        }]
+    })
+)
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+# Attach AWS Lambda Basic Execution Role
+aws.iam.RolePolicyAttachment("lambdaBasicExecution",
+    role=lambda_role.name,
+    policy_arn="arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+)
+
+# Zip Lambda function
+lambda_archive = pulumi.AssetArchive({
+    ".": pulumi.FileArchive("./lambda")  # Ensure the correct path
+})
+
+
+# Create Lambda Function
+lambda_function = aws.lambda_.Function("additionLambda",
+    runtime="python3.9",
+    role=lambda_role.arn,
+    handler="addition.lambda_handler",
+    code=lambda_archive,
+)
+
+# Export Lambda Function Name
+pulumi.export("lambda_function_name", lambda_function.name)
